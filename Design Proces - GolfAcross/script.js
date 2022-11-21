ba4a6ec4196f277ba2eb97dc0a94b8cf91b17c23
MagnusCD <53862351+MagnusCD@users.noreply.github.com>
2022-11-21 11:24:06 -0800

    Add files via upload

---
+++ Design Proces - GolfAcross/script.js
@@ -0,0 +1,71 @@
+/*Kildehttps://www.w3schools.com/howto/howto_js_topnav_responsive.asp*/
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function changeColor() {
+  document.getElementById("box-button").style.backgroundColor = "green";
+}
+function changeColor2() {
+  document.getElementById("box-button2").style.backgroundColor = "green";
+}
+function changeColor3() {
+  document.getElementById("box-button3").style.backgroundColor = "green";
+}
+function changeColor4() {
+  document.getElementById("box-button4").style.backgroundColor = "green";
+}
+function changeColor5() {
+  document.getElementById("box-button5").style.backgroundColor = "green";
+}
+function changeColor6() {
+  document.getElementById("box-button6").style.backgroundColor = "green";
+}
+function changeColor7() {
+  document.getElementById("box-button7").style.backgroundColor = "green";
+}
+function changeColor8() {
+  document.getElementById("box-button8").style.backgroundColor = "green";
+}
+function changeColor9() {
+  document.getElementById("box-button9").style.backgroundColor = "green";
+}
+function changeColor10() {
+  document.getElementById("box-button10").style.backgroundColor = "green";
+}
+function changeColor11() {
+  document.getElementById("box-button11").style.backgroundColor = "green";
+}
+function changeColor12() {
+  document.getElementById("box-button12").style.backgroundColor = "green";
+}
+function changeColor13() {
+  document.getElementById("box-button13").style.backgroundColor = "green";
+}
+function changeColor14() {
+  document.getElementById("box-button14").style.backgroundColor = "green";
+}
+function changeColor15() {
+  document.getElementById("box-button15").style.backgroundColor = "green";
+}
+function changeColor16() {
+  document.getElementById("box-button16").style.backgroundColor = "green";
+}
+function changeColor17() {
+  document.getElementById("box-button17").style.backgroundColor = "green";
+}
+function changeColor18() {
+  document.getElementById("box-button18").style.backgroundColor = "green";
+}
+function changeColor19() {
+  document.getElementById("box-button19").style.backgroundColor = "green";
+}
+function changeColor20() {
+  document.getElementById("box-button20").style.backgroundColor = "green";
+}
